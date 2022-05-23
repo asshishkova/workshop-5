6255c57049af387aad984d35d3bc642336a71777
Anna Shishkova <asshishkova@gmail.com>
2022-05-23 22:06:37 +0300

    Solve the task

--- mondayu-logger/index.js
+++ mondayu-logger/index.js
@@ -4,4 +4,9 @@ import { createWriteStream } from 'fs';
 // step 1: use this example: https://melvingeorge.me/blog/save-logs-to-files-nodejs
 // export your monday logger here
 
-// step 2: rename the package and publish 
+export const myLogger = new Console({
+  stdout: createWriteStream("normalStdout.txt", {flags: 'a'}),
+});
+myLogger.log("Hello 😃");
+
+// step 2: rename the package and publish
--- weather-app/index.js
+++ weather-app/index.js
@@ -1,17 +1,32 @@
 import { AsyncWeather } from "@cicciosgamino/openweather-apis";
 import http from "http";
 // import your own logger
+import { myLogger } from "mondayu-logger-assh";
 
 // step 1: install lib and print temp to console
-const apiKey = "<<your api key>>";
+const apiKey = "14ff40f2254b5c3396c112b5bdc582b8";
 const weatherInitializer = new AsyncWeather();
 const weatherAPI = await weatherInitializer;
+
+weatherAPI.setApiKey(apiKey);
+weatherAPI.setCityId(293397);
+const temp = await weatherAPI.getTemperature();
+console.log("temp", temp);
 
 // step 2: server
 // create your server here from http module
 // example in the slides or here: https://nodejs.org/en/knowledge/HTTP/servers/how-to-create-a-HTTP-server/
 
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  // res.end('hi')
+  res.end(JSON.stringify({"temp": temp}));
+}
+const server = http.createServer(requestListener);
+server.listen(8080);
 // start your server
 
 // step 3: install your logger and log temp and server start
 // log that the service has started
+myLogger.log("server started");
+myLogger.log("temp", temp);
